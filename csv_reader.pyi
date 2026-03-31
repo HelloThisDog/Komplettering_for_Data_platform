90ed8f2ef0c099a522c1d75b45a84d6eca40a53e
Megan Loe <meganhelenaloe@gmail.com>
2026-03-31 18:10:55 +0200

    data and now main body of task done, api next

--- csv_reader.pyi
+++ csv_reader.pyi
@@ -1,2 +1,11 @@
 import pandas as pd
-from fastapi import fastAPI
+
+if __name__ == "__main__":
+    #reads data
+    df = pd.DataFrame(pd.read_csv("gen1_pokemon_full.csv", sep=",")) #ngl I thought it was gonna be ";"" not "," caught me off guard for a min
+
+    high_speed = df.sort_values(by="speed", ascending=False).iloc[:5, [1, 8]] #limits the list down to the top 5 pokemon with highest speed
+    high_speed.to_csv("top_5_speed.csv") #produces the csv
+    
+    
+    
---
+++ main.py
@@ -0,0 +1,5 @@
+def main():
+    print("this should work")
+
+if __name__ == "__main__":
+    main()
