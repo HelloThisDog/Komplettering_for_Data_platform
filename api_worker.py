03a5a998b93bf092f89517cb9746febc89c0ebb1
Megan Loe <meganhelenaloe@gmail.com>
2026-04-30 15:46:19 +0200

    nothing fucking works, scrapping everything

--- api_worker.py
+++
@@ -1,8 +0,0 @@
-from typing import Union
-from fastapi import FastAPI, File, UploadFile
-
-app = FastAPI(title="top 5 API")
-
-@app.get("/readfileplz/{top_5_speed.csv}")
-def create_upload_file(file: UploadFile):
-    return {"top five 5": "file.top_5_speed.csv"} 
