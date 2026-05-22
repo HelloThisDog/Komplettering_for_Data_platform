0f16078dc40831c9d895d760222af63e4a699d2e
Megan Loe <meganhelenaloe@gmail.com>
2026-05-22 14:29:33 +0200

    debugging done with help of ChatGPT, think I'm done

--- api.py
+++ api.py
@@ -12,12 +12,12 @@ if __name__ == "__main__":
     #reads data
     df = pd.DataFrame(pd.read_csv("gen1_pokemon_full.csv", sep=",")) #ngl I thought it was gonna be ";"" not "," caught me off guard for a min
 
-    high_speed = df.sort_values(by="speed", ascending=False).iloc[:5, [1, 8]] #limits the list down to the top 5 pokemon with highest speed
-    high_speed.to_csv("top_5_speed.csv") #produces the csv
+    high_speed = df.sort_values(by="speed", ascending=False).iloc[:5, [0, 1, 8]] #limits the list down to the top 5 pokemon with highest speed
+    high_speed.to_csv("top_5_speed.csv", index=False) #produces the csv
 
 DATA_PATH = Path(__file__).parent/"top_5_speed.csv"
 
-KAFKA_BOOTSTRAP_SERVERS = os.getenv("KAFKA_BOOTSTRAP_SERVERS", "kafka:9092")
+KAFKA_BOOTSTRAP_SERVERS = os.getenv("KAFKA_BOOTSTRAP_SERVERS", "broker:29092")
 PRODUCTS_TOPIC = os.getenv("PRODUCTS_TOPIC", "products.created")
 
 
@@ -26,8 +26,8 @@ PRODUCTS_TOPIC = os.getenv("PRODUCTS_TOPIC", "products.created")
 async def lifespan(app: FastAPI):
     app.state.kafka_producer = KafkaProducer (
         bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS,
-        values_serializer=lambda v: json.dumps(v).encode("uft-8"),
-        key_serializer=lambda k: k.encode("uft-8") if k else None,)
+        value_serializer=lambda v: json.dumps(v).encode("utf-8"),
+        key_serializer=lambda k: k.encode("utf-8") if k else None,)
 
     yield
 
@@ -49,19 +49,23 @@ def root():
 def post_product():
     df = pd.read_csv(DATA_PATH)
 
-    event = {
-        "type": "product.created",
-        "product_id": df["id"],
-        "name": df["name"],
-        "speed": str(df["speed"]),
-    }
+    events = []
+    for _, row in df.iterrows():
+        event = {
+            "type": "product.created",
+            "product_id": int(row["id"]),
+            "name": row["name"],
+            "speed": int(row["speed"]),
+        }
 
-    app.state.kafka_producer.send(
-        PRODUCTS_TOPIC,
-        key=str(df["id"]),
-        value=event
-    )
+        app.state.kafka_producer.send(
+            PRODUCTS_TOPIC,
+            key=str(row["id"]),
+            value=event
+        )
+
+        events.append(event)
 
     app.state.kafka_producer.flush()
 
-    return df
+    return events
